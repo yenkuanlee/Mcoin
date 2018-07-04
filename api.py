6111bbe7dd5be4de02dc47cbb793f69a7e024d7c
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-07-04 11:38:21 +0800

    finish post api version

--- api.py
+++ api.py
@@ -55,6 +55,30 @@ def get_info():
     Odict['Balance'] = output
     return json.dumps(Odict)
 
+@app.route('/GetInfoX', methods=['POST'])
+def get_infoX():
+    Email = request.form['Email']
+    cmd = "python3 /home/localadmin/yenkuanlee/Mcoin/User/GetInfo.py "+Email
+    output = subprocess.check_output(cmd, shell=True)
+    output = output.decode("utf-8")
+    Joutput = json.loads(output)
+    Odict = dict()
+    Odict['Ehash'] = Joutput[0]
+    Odict['StudentID'] = Joutput[1]
+    #Odict["IPFSHASH"] = Joutput[2]
+    cmd = "timeout 10 ipfs object get "+Joutput[2]
+    output = subprocess.check_output(cmd, shell=True)
+    output = output.decode("utf-8")
+    transaction_record = json.loads(output)
+    Odict['TransactionRecord'] = transaction_record
+    Odict['role'] = Joutput[3]
+    Odict['nounce'] = Joutput[4]
+    cmd = "python3 /home/localadmin/yenkuanlee/Mcoin/Balance/GetBalance.py "+Odict['Ehash']
+    output = subprocess.check_output(cmd, shell=True)
+    output = int(output.decode("utf-8"))
+    Odict['Balance'] = output
+    return json.dumps(Odict)
+
 @app.route('/GetBalance')
 def get_balance():
     args = request.args
@@ -78,11 +102,26 @@ def send_raw_transaction():
     output = subprocess.check_output(cmd, shell=True)
     return output
 
+@app.route('/sendRawTransactionX', methods=['POST'])
+def send_raw_transactionX():
+    RAW_TRANSACTION = request.form['RAW_TRANSACTION']
+    cmd = "python3 /home/localadmin/yenkuanlee/Mcoin/Transaction/sendRawTransaction.py "+RAW_TRANSACTION
+    output = subprocess.check_output(cmd, shell=True)
+    return output
+
 @app.route('/TransactionRecord')
 def transaction_record():
     args = request.args
     Email = args['Email']
     T = args['T']
+    cmd = "python3 /home/localadmin/yenkuanlee/Mcoin/Transaction/TransactionRecord.py "+Email+" "+T
+    output = subprocess.check_output(cmd, shell=True)
+    return output
+
+@app.route('/TransactionRecordX', methods=['POST'])
+def transaction_recordX():
+    Email = request.form['Email']
+    T = request.form['T']
     cmd = "python3 /home/localadmin/yenkuanlee/Mcoin/Transaction/TransactionRecord.py "+Email+" "+T
     output = subprocess.check_output(cmd, shell=True)
     return output
