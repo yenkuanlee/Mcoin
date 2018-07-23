a9b43b7368e2949d3b504541d2b4f7a6ea90e156
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-07-23 10:52:54 +0800

    multi raw transaction

--- Transaction/sendRawTransaction.py
+++ Transaction/sendRawTransaction.py
@@ -1,7 +1,14 @@
 from web3 import Web3, HTTPProvider
+import json
 import sys
+
 Rhash = sys.argv[1]
+Tlist = list()
 
 w3 = Web3(HTTPProvider('http://localhost:3000'))
-T = w3.eth.sendRawTransaction(Rhash)
-print(T)
+
+tmp = Rhash.split(",")
+for x in tmp:
+    T = w3.eth.sendRawTransaction(x)
+    Tlist.append(T)
+print(json.dumps(T))
